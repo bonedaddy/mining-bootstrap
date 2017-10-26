b0b19f0fca624e3c35f2c3d8fe3ceecda01427ea
Postables <alextrottier2014@gmail.com>
2017-10-26 16:30:41 -0700

    updates

--- runOnBoot.sh
+++ runOnBoot.sh
@@ -6,6 +6,12 @@
 
 bootScriptDir="/boot_scripts"
 logDir="/logs"
+rigName="$(hostname)"
+poolUrl="stratum+tcp://hub.miningpoolhub.com:20507"
+poolUser="postables.$rigName"
+poolPassword="password"
+minerAlgo="lyra2v2"
+
 
 # Execute underclock
 mv "$logDir/boot.log" "$logDir/previousBoot.log"
@@ -16,15 +22,8 @@ bash -x "$bootScriptDir/underclock.sh"           >> "$logDir/boot.log"
 if [[ "$?" -eq 0 ]]; then
 	printf "[INFO] Underlock Script Finished \n\n"   >> "$logDir/boot.log"
 else
-	printf "[ERROR] Underclock script failed to start, going to exit"
+	printf "[ERROR] Underclock script failed to start, going to exit" >> "$logDir/boot.log"
+	exit
 fi
 
-printf "[INFO] Starting Miner Launch Script\n\n" >> "$logDir/boot.log"
-# Note: We must use /bin/sh to do this, because if we use /bin/bash, it will pass in the whole argument string to screen as a single option, whereas with /bin/sh it will pass it in as a list of different words. Also note that the "$0" refers to the script name, so we're going to call that script independently since it will be using a different shell
-"$bootScriptDir/minerStart.sh" 
-
-if [[ "$?" -eq 0 ]]; then
-	printf "[INFO] Miner launched successfully, going to exit\n\n" >> "$logDir/boot.log"
-else
-	printf "[ERROR] Miner not launched successfully\n\n" >> "$logDir/boot.log"
-fi
+ccminer --url="$poolUrl" --user "$poolUser" --pass "$poolPassword" --algo="$minerAlgo" &
