508f08d20a464a2effbcf1663133c9f8ab90107a
Postables <alextrottier2014@gmail.com>
2017-11-18 21:15:28 -0800

    a

--- price_calc.py
+++ price_calc.py
@@ -1,5 +1,7 @@
 import re
 import sys
+import urllib.request
+import json
 
 if len(sys.argv) < 2:
     print("Improper invocation")
@@ -24,19 +26,29 @@ with open(input_prices) as fh:
             date, price = line.split(',')
             usdPerDay[date] = price
 
-template_message_header = "DATE\t\tVTC-MINED\t%s-VALUE-PER-COIN\tTOTAL-USD-VALUE" % coin_name
+template_message_header = "DATE\t\t%s-MINED\t%s-VALUE-PER-COIN\tTOTAL-CAD-VALUE" % (coin_name,coin_name)
 template_message_barrier = "====\t\t=========\t==================\t==============="
-template_message = "{date}\t{vtcmined}\t\t{usdvaluepercoin}\t\t\t{usdvaluetotal}"
+template_message = "{date}\t{vtcmined}\t\t{cadvaluepercoin}\t\t\t{cadvaluetotal}"
 message_list = []
+
+# calculate USD value
+raw_data = urllib.request.urlopen('https://api.fixer.io/latest?base=usd')
+data = raw_data.read()
+encoding = raw_data.info().get_content_charset('utf-8')
+json_data = json.loads(data.decode(encoding))
+usdToCad = json_data['rates']['CAD']
+
 
 for date in vtcPerDay.keys():
     vtc_mined = vtcPerDay[date]
     vtc_mined = float(vtc_mined)
     usd_value_per_vtc = usdPerDay[date]
+    cad_value_per_vtc = float(usd_value_per_vtc) * float(usdToCad)
     total_usd_value = float(vtc_mined) * float(usd_value_per_vtc)
+    total_cad_value = float(total_usd_value) * float(usdToCad)
     usd_value_per_vtc = float(usd_value_per_vtc)
     total_usd_value = float(total_usd_value)
-    msg = template_message.format(date=date, vtcmined='%0.2f' %vtc_mined, usdvaluepercoin='%0.2f' % usd_value_per_vtc,usdvaluetotal='%0.2f' % total_usd_value)
+    msg = template_message.format(date=date,vtcmined='%0.2f' %vtc_mined,cadvaluepercoin='%0.2f' % cad_value_per_vtc,cadvaluetotal='%0.2f' % total_cad_value)
     message_list.append(msg)
 
 with open(report_file, 'a') as fh:
