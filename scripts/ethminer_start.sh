5de60bb49eb651e5ec2e04fa9da113928c6051b3
potsables <alextrottier2014@gmail.com>
2018-05-30 20:25:51 -0700

    added ethminer stats script

--- scripts/ethminer_start.sh
+++ scripts/ethminer_start.sh
@@ -5,4 +5,4 @@ WORKER_NAME=$(hostname)
 PORT="20535"
 URL="stratum2+tcp://$USERNAME.$WORKER_NAME:x@us-east.ethash-hub.miningpoolhub.com:$PORT"
 
-/boot_scripts/bin/ethminer -U -P "$URL"
+/boot_scripts/bin/ethminer -U -P "$URL" --api-port -6767
---
+++ scripts/ethminer_stats.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+
+case "$1" in 
+
+    miner_getstat1)
+        echo '{"id":17,"jsonrpc":"2.0","method":"miner_getstat1"}'  |  netcat "$2" 6767
+        ;;
+esac
+
+
+###
+#"9.3 - ETH"				- miner version.
+#"21"					- running time, in minutes.
+#"182724"				- total ETH hashrate in MH/s, number of ETH shares, number of ETH rejected shares.
+#"30502;30457;30297;30481;30479;30505"	- detailed ETH hashrate for all GPUs.
+#"0;0;0"					- total DCR hashrate in MH/s, number of DCR shares, number of DCR rejected shares.
+#"off;off;off;off;off;off"		- detailed DCR hashrate for all GPUs.
+#"53;71;57;67;61;72;55;70;59;71;61;70"	- Temperature and Fan speed(%) pairs for all GPUs.
+#"eth-eu1.nanopool.org:9999"		- current mining pool. For dual mode, there will be two pools here.
+#"0;0;0;0"				- number of ETH invalid shares, number of ETH pool switches, number of DCR invalid shares, number of DCR pool switches.
+###
