21d7d6d917ded247169edd0f406a717f0639e320
potsables <alextrottier2014@gmail.com>
2018-05-31 18:26:28 -0700

    cleaned up the scripts

--- scripts/nvidia_smi_parser.sh
+++ scripts/nvidia_smi_parser.sh
@@ -7,24 +7,22 @@
 QUERY_MODE="$1"
 # the gpu to pull metrics from
 SELECTED_GPU_NUMBER="$2"
-# cleans up output from list-gpu into an iterable format
-GPU_NUMBERS=$(nvidia-smi --list-gpus | awk '{print $2}' | tr -d ':')
 # retrieves the total number of GPUs
-GPU_COUNT=$(nvidia-smi --query-gpu=count --format=csv,noheader)
-
+GPU_COUNT=$(nvidia-smi --query-gpu=count --format=csv,noheader | head -n 1)
+let MAX=$(($GPU_COUNT-1))
 # make sure that a non-empty parameter was given
 if [[ "$SELECTED_GPU_NUMBER" == "" ]]; then
     echo "[ERROR] No valid gpu number provided"
-        echo "Invalid invocation"
-        echo "./nvidia_smi_parser.sh [temp | draw | fan_speed | gpu_utilization_percent| mem_utilization_percent | gpu_clock_mhz]  <gpu-number>"
+    echo "Invalid invocation"
+    echo "./nvidia_smi_parser.sh [temp | draw | fan_speed | gpu_utilization_percent| mem_utilization_percent | gpu_clock_mhz]  <gpu-number>"
     exit 1
 fi
 
 # we need to compare against 1 below maximum since gpu is indexed starting at 0
-if [[ "$SELECTED_GPU_NUMBER" -gt $(($(echo "$GPU_COUNT") - 1)) ]]; then
+if [[ "$SELECTED_GPU_NUMBER" -gt "$MAX" ]]; then
     echo "[ERROR] provided gpu number greater than gpu count"
-        echo "Invalid invocation"
-        echo "./nvidia_smi_parser.sh [temp | draw | fan_speed | gpu_utilization_percent| mem_utilization_percent | gpu_clock_mhz]  <gpu-number>"
+    echo "Invalid invocation"
+    echo "./nvidia_smi_parser.sh [temp | draw | fan_speed | gpu_utilization_percent| mem_utilization_percent | gpu_clock_mhz]  <gpu-number>"
     exit 1
 fi
 
@@ -42,7 +40,7 @@ get_power_draw() {
 
 get_fan_speed() {
     SPEED=$(nvidia-smi -i "$1" --query-gpu=fan.speed --format=csv,noheader)
-    echo "$SPEED"
+    echo "$SPEED" | tr -d '%'
 }
 
 get_gpu_utilization_percent() {
@@ -59,6 +57,12 @@ get_gpu_clock_mhz() {
    CLOCK=$(nvidia-smi -i "$1" -d CLOCK -q | grep '^    Clocks$' -A 4 | sed '1d' | grep Graphics  | awk -F ':' '{print $2}' | awk '{print $1}')
    echo "$CLOCK"
 }
+
+get_memory_clock_mhz() {
+    CLOCK=$(nvidia-smi -i "$1" -d CLOCK -q | grep '^    Clocks$' -A 4 | sed '1d' | grep Memory  | awk -F ':' '{print $2}' | awk '{print $1}')
+    echo "$CLOCK"
+}
+
 case "$QUERY_MODE" in 
 
     # query is for GPU temp
@@ -69,28 +73,30 @@ case "$QUERY_MODE" in
     draw)
         get_power_draw "$SELECTED_GPU_NUMBER"
         ;;
+
     fan_speed)
-	get_fan_speed "$SELECTED_GPU_NUMBER"
-	;;
+	    get_fan_speed "$SELECTED_GPU_NUMBER"
+	    ;;
+
     gpu_utilization_percent)
-	get_gpu_utilization_percent "$SELECTED_GPU_NUMBER"
+	    get_gpu_utilization_percent "$SELECTED_GPU_NUMBER"
         ;;
+
     mem_utilization_percent)
-	get_memory_utilization_percent "$SELECTED_GPU_NUMBER"
+	    get_memory_utilization_percent "$SELECTED_GPU_NUMBER"
         ;;
+
     gpu_clock_mhz)
         get_gpu_clock_mhz "$SELECTED_GPU_NUMBER"
         ;;
+
+    mem_clock_mhz)
+        get_memory_clock_mhz "$SELECTED_GPU_NUMBER"
+        ;;
+        
     *)
         echo "Invalid invocation"
         echo "./nvidia_smi_parser.sh [temp | draw | fan_speed | gpu_utilization_percent| mem_utilization_percent | gpu_clock_mhz]  <gpu-number>"
         exit 1
         ;;
 esac            
-
-
-
-
-# Useful information
-# To retrieve the current clocks
-# nvidia-smi -i 0 -d CLOCK -q | grep '^    Clocks$' -A 4 | sed '1d
