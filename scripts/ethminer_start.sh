856de702702a10eba44a51a95132403d1fbaade5
potsables <alextrottier2014@gmail.com>
2018-06-06 19:08:46 -0700

    added log redirect

--- scripts/ethminer_start.sh
+++ scripts/ethminer_start.sh
@@ -5,4 +5,4 @@ WORKER_NAME=$(hostname)
 PORT="20535"
 URL="stratum2+tcp://$USERNAME.$WORKER_NAME:x@us-east.ethash-hub.miningpoolhub.com:$PORT"
 
-/boot_scripts/bin/ethminer -U -P "$URL" --api-port -6767 | tee --append /var/log/ethminer.log
+/boot_scripts/bin/ethminer -U -P "$URL" --api-port -6767 2>&1 | tee --append /var/log/ethminer.log
