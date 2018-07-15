beff1a758cae05a6bc65c159bbe8750de14159f3
potsables <alextrottier2014@gmail.com>
2018-07-15 06:09:01 +0200

    workgin on tests

--- src/reports/main_test.go
+++ src/reports/main_test.go
@@ -1,7 +1,9 @@
 package reports_test
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
 
@@ -20,5 +22,15 @@ func TestManager(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(resp)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var intf interface{}
+	err = json.Unmarshal(body, &intf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(intf)
+
 }
