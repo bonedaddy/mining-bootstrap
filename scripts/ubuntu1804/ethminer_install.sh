e7b60149669378505ca26649eaaa7a681f1525ef
potsables <alextrottier2014@gmail.com>
2018-08-18 20:16:01 -0700

    install process changed

--- scripts/ubuntu1804/ethminer_install.sh
+++ scripts/ubuntu1804/ethminer_install.sh
@@ -6,12 +6,16 @@ cd ~ || exit
 
 echo "[INFO] Downloading ethminer"
 git clone https://github.com/ethereum-mining/ethminer.git
-cd ethmienr || exit
+cd ethminer || exit
 
 echo "[INFO] Installing dbus development libraries"
 sudo apt install libdbus-1-dev
 echo "[INFO] Installing cmake"
 sudo apt install cmake
+
+echo "[INFO] Running GCC fix for ubuntu 18.04"
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 10
 
 echo "[INFO] Updating submodules"
 git submodule update --init --recursive
@@ -24,6 +28,3 @@ cmake .. -DETHASHCUDA=ON -DETHASHCL=OFF -DAPICORE=ON -DETHDBUS=ON -DBINKERN=OFF
 
 echo "[INFO] Building ethminer project"
 cmake --build .
-
-echo "[INFO] Installing executable"
-sudo make install
